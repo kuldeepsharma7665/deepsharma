3dbf4f595e053f4cb8b0f45ded641b5e6b9628b8
kuldeep sharma <kuldeepsharmawab@gmail.com>
2021-12-18 19:56:47 +0530

    HELLO

---
+++ package-info.java
@@ -0,0 +1,15 @@
+package deepshrma;
+import java.util.Scanner;
+class Demo1{
+	
+	
+	public static void main(String[]arg) {
+		
+		Scanner sc=new Saccner(System.in);
+		System.out.println("Please enter your name : -");
+		String name= sc.next();
+		System.out.println("Mr "+name);
+		
+		
+	}
+}
